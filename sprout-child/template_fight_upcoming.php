5da534c14f6fcc189986ff3fdae9de73eeeec7cf
joe Carrano <joe@jcarrano.com>
2016-12-22 09:41:21 -0500

    Addressing issue with sort on a date

--- sprout-child/template_fight_upcoming.php
+++ sprout-child/template_fight_upcoming.php
@@ -8,8 +8,8 @@ $currentDate = date('Ymd');
 $args = array(
 
     'post_type' 		=> 'fight_events',
-    'meta_sort_key' 	=> 'event_date',
-    'orderby'			=> 'meta_sort_key',
+    'meta_key' 	=> 'event_date',
+    'orderby'			=> 'meta_value',
 	'meta_query'		=> array(
 		'relation'		=> 'AND',
 		array(
